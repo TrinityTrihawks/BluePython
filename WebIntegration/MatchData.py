f52dd574cc2f94f96eaf8e16c9b2f3e8880709c8
Waweru Kariuki <lion@waposta.com>
2017-04-02 15:03:08 -0500

    Began to use tbapy library

--- WebIntegration/MatchData.py
+++ WebIntegration/MatchData.py
@@ -6,25 +6,30 @@ Created on Mar 28, 2017
 import http.client
 import json
 import ssl
+import tbapy
 #import requests
 
-URL = 'http://www.thebluealliance.com/api/v2/'
-HEADER_KEY = 'X-TBA-App-Id'
+URL = "http://www.thebluealliance.com/api/v2/"
+HEADER_KEY = "X-TBA-App-Id"
 HEADER_VAL = 'frc4215:data-analysis:.1'
 
 
+def api_is_up():
+    conn = http.client.HTTPConnection(URL,80)
+    conn.request('GET',"/status",{HEADER_KEY : HEADER_VAL})
+    response = conn.getresponse()
+    return response.read()
 
 def get_event_teams(event_key):
-    request = http.client.HTTPConnection(URL)
-    #request.add_header(HEADER_KEY, HEADER_VAL)
-    response = http.client.HTTPConnection(URL)
-    jsonified = json.loads(response.read().decode("utf-8"))
+    tba = tbapy.TBA(HEADER_VAL)
+    jsonified = tba.event_teams(event_key)
     teams = []
-
     for team in jsonified:
-        teams.append(team['key'])
+        teams.append(team["key"])
 
     return teams
 
-teams = get_event_teams("qcmo")
+teams = get_event_teams('2010sc')
 print(teams)
+#up = api_is_up()
+#print(up)
